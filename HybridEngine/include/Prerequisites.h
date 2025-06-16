ff50a78aa9d593cb5f39edbc5595f839fe56c118
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2025-06-16 11:15:26 -0600

    Creation Shader Program and Input Layout

--- HybridEngine/include/Prerequisites.h
+++ HybridEngine/include/Prerequisites.h
@@ -65,3 +65,9 @@ ExtensionType {
   PNG = 1,
   JPG = 2
 };
+
+enum 
+ShaderType {
+  VERTEX_SHADER = 0,
+  PIXEL_SHADER = 1
+};
---
+++ HybridEngine/include/ShaderProgram.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Prerequisites.h"
+#include "InputLayout.h"
+
+class Device;
+class DeviceContext;
+
+class 
+ShaderProgram {
+public:
+	ShaderProgram ()  = default;
+	~ShaderProgram () = default;
+
+  HRESULT 
+  init(Device& device, 
+       const std::string& fileName, 
+       std::vector<D3D11_INPUT_ELEMENT_DESC> Layout);
+
+  void 
+  update();
+  
+  void 
+  render(DeviceContext& deviceContext);
+  
+  void 
+  destroy();
+
+  HRESULT
+  CreateInputLayout(Device & device, std::vector<D3D11_INPUT_ELEMENT_DESC> Layout);
+
+  HRESULT
+  CreateShader(Device & device, ShaderType type);
+
+  HRESULT 
+  CompileShaderFromFile(char* szFileName, 
+                        LPCSTR szEntryPoint, 
+                        LPCSTR szShaderModel, 
+                        ID3DBlob** ppBlobOut);
+
+public:
+  ID3D11VertexShader* m_VertexShader = nullptr;
+  ID3D11PixelShader* m_PixelShader = nullptr;
+  //InputLayout m_inputLayout;
+private:
+  std::string m_shaderFileName;
+  ID3DBlob* m_vertexShaderData = nullptr;
+  ID3DBlob* m_pixelShaderData = nullptr;
+};
