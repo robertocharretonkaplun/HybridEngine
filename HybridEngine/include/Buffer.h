28dd463a16d43d9bc6eb302538ce7362557727e3
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2025-06-23 11:52:16 -0600

    Buffer implementation

---
+++ HybridEngine/include/Buffer.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "Prerequisites.h"
+
+class Device;
+class DeviceContext;
+class MeshComponent;
+
+class 
+Buffer {
+public:
+	Buffer()  = default;
+	~Buffer() = default;
+
+	 // Inicializa Vertex e Index Buffers 
+  HRESULT
+  init(Device& device, const MeshComponent& mesh, unsigned int bindFlag);
+
+  // Inicializa Constant Buffers
+  HRESULT
+  init(Device& device, unsigned int ByteWidth);
+
+  // Actualiza los constant buffers a nivel logico
+	void
+	update(ID3D11Resource* pDstResource,
+         unsigned int DstSubresource,
+         const D3D11_BOX* pDstBox,
+         const void* pSrcData,
+         unsigned int SrcRowPitch,
+         unsigned int SrcDepthPitch);
+
+  // Actualiza en render el Vertex, Index y Constant Buffer
+  void
+  render(DeviceContext& deviceContext,
+         unsigned int StartSlot,
+         unsigned int NumBuffers,
+         bool setPixelShader = false,
+         DXGI_FORMAT format = DXGI_FORMAT_UNKNOWN);
+
+  HRESULT
+  createBuffer(Device& device, 
+               D3D11_BUFFER_DESC& desc, 
+               D3D11_SUBRESOURCE_DATA* initData);
+
+
+private:
+	ID3D11Buffer* m_buffer = nullptr;
+	unsigned int m_stride = 0;
+	unsigned int m_offset = 0;
+	unsigned int m_bindFlag = 0;
+};
---
+++ HybridEngine/include/MeshComponent.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Prerequisites.h"
+//#include "ECS\Component.h"
+
+class DeviceContext;
+
+class
+  MeshComponent /*: public Component*/ {
+public:
+  MeshComponent() : m_numVertex(0), m_numIndex(0)/*, Component(ComponentType::MESH)*/ {}
+
+  virtual
+  ~MeshComponent() = default;
+
+  /**
+   * @brief Actualiza el actor.
+   * @param deltaTime El tiempo transcurrido desde la última actualización.
+   * @param deviceContext Contexto del dispositivo para operaciones gráficas.
+   */
+  void
+  update(float deltaTime) /*override*/ {}
+
+  /**
+   * @brief Renderiza el actor.
+   * @param deviceContext Contexto del dispositivo para operaciones gráficas.
+   */
+  void
+  render(DeviceContext& deviceContext) /*override*/ {}
+public:
+  std::string m_name;
+  std::vector<SimpleVertex> m_vertex;
+  std::vector<unsigned int> m_index;
+  int m_numVertex;
+  int m_numIndex;
+};
