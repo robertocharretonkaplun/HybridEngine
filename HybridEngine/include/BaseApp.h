0be353d611c72a8e3db394df9f33c90daa961030
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2025-07-22 11:42:08 -0600

    FBX implementation

--- HybridEngine/include/BaseApp.h
+++ HybridEngine/include/BaseApp.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "Prerequisites.h"
 #include "Window.h"
+#include "Device.h"
+#include "SwapChain.h"
+#include "DeviceContext.h"
+#include "Texture.h"
+#include "RenderTargetView.h"
+#include "DepthStencilView.h"
+#include "Viewport.h"
+#include "InputLayout.h"
+#include "ShaderProgram.h"
+#include "Buffer.h"
+#include "MeshComponent.h"
+#include "BlendState.h"
+#include "DepthStencilState.h"
 
 class
 BaseApp {
---
+++ HybridEngine/include/ModelLoader.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Prerequisites.h"
+#include "MeshComponent.h"
+#include "fbxsdk.h"
+
+class 
+ModelLoader {
+public:
+	ModelLoader() = default;
+	~ModelLoader() = default;
+
+	/* OBJ MODEL LOADER*/
+
+	/* FBX MODEL LOADER*/
+	bool
+	InitializeFBXManager();
+
+	bool 
+	LoadFBXModel(const std::string & filePath);
+
+	void 
+  ProcessFBXNode(FbxNode* node);
+
+  void 
+  ProcessFBXMesh(FbxNode* node);
+
+  void 
+  ProcessFBXMaterials(FbxSurfaceMaterial* material);
+
+	std::vector<std::string> 
+  GetTextureFileNames() const { return textureFileNames; }
+
+private:
+	FbxManager* lSdkManager;
+	FbxScene* lScene;
+	std::vector<std::string> textureFileNames;
+public:
+	std::string modelName;
+	std::vector<MeshComponent> meshes;
+};
+
